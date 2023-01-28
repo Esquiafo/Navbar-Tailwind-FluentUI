8d7b40288dbeee523ba77e93bb4457583ecd30b5
Leonardo Capone <57331052+Esquiafo@users.noreply.github.com>
2023-01-28 05:04:01 -0300

    050328012023

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,43 +1,38 @@
-
 import React,{useState,useEffect} from "react";
 import BigNavbar from "./BigNav"
-import { Button, Popover, PopoverSurface, PopoverTrigger, PopoverProps } from "@fluentui/react-components";
+import { Button, Menu, MenuList, MenuTrigger } from "@fluentui/react-components";
 import { ChevronDown28Filled} from '@fluentui/react-icons';
 
 
 export default function Navbar() {
     const [rotate, showRotate] = useState('rotate-0');
+    const [open, setOpen] = useState(false);
     function showBar() {
       rotate == 'rotate-0' ? showRotate('rotate-180') : showRotate('rotate-0'); 
+      open == false ? setOpen(true) : setOpen(false)
   } 
-  const [open, setOpen] = useState(false);
+
   useEffect(() => {
     setOpen(true)
   }, [])
-  const handleOpenChange: PopoverProps['onOpenChange'] = (e, data) => setOpen(data.open || false);
+
 
     return(
-     
-    <Popover open={open} onOpenChange={handleOpenChange}>
-    <PopoverTrigger   disableButtonEnhancement>
+      <Menu open={open}>
+    
+    <MenuTrigger disableButtonEnhancement>
     <Button  onClick={function(){showBar()}} ><ChevronDown28Filled className={`${rotate} transition duration-700 `} /></Button>
-    </PopoverTrigger>
-
-    <PopoverSurface className="transition duration-700" style={{border: 'none'}}>
+    </MenuTrigger>
+    
+    <MenuList className="mt-8 relative transition duration-700" style={{border: 'none'}}>
         <BigNavbar  />
-    </PopoverSurface>
-  </Popover>
+    </MenuList>
+  
        
-      
+  </Menu>
 
         
-     
- 
- 
-
-       
-
- 
         
     )
 }
+
